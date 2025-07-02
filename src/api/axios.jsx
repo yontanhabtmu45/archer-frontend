110fad17ceaa5c710de23f7fca077e2c459d1de5
Jhan-hab <201286123+yontanhabtmu45@users.noreply.github.com>
2025-07-02 16:13:55 +0300

    deployment

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:2716/api'
+    // baseURL: 'http://localhost:2716/api'
+    baseURL: 'https://backend-archer.onrender.com'
 })
 
 export default axiosInstance
